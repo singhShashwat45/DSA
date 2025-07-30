4d17608728a0164cfc093947f68019382c3d97cb
Shashwat Singh <shashwat0045@gmail.com>
2025-07-30 15:15:08 +0530

    Linked List

---
+++ linked_list/Delete_Node_in_a_Linked_List.cpp
@@ -0,0 +1,16 @@
+//Problem link - https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+struct ListNode {
+    int val;
+     ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+ class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next=node->next->next;
+    }
+};
