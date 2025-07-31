c4a6d68fe6b70fe3e19747b5d3558e798119f7d2
Shashwat Singh <shashwat0045@gmail.com>
2025-07-31 11:43:21 +0530

    recursion

--- recursion/Subset_Sums.cpp
+++ recursion/Subset_Sums.cpp
@@ -1,34 +1,7 @@
-geeksforgeeks
-Courses
-Tutorials
-Practice
-Jobs
-S
-51
+#include <iostream>
+#include <vector>
+using namespace std;
 
-Search...
-
-
-
-Upgrade to Premium for Doubt Support across all your problems and courses.
-
-Explore Premium
-Redirection Icon
-Refresh
-
-Time (IST)	Status	Marks	Lang	Test Cases	Code
-2024-08-19 11:17:48	Correct	
-4
-cpp	1115 / 1115	View
-C++ (12)
-
-
-
-        
-
-
-
-Custom Input
 class Solution {
   public:
     void func(vector<int> arr, int n, vector<int>&ans, int ind, int sum){
