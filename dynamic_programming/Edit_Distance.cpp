63debd06344f995a98346ef05f7bb44021e9a3c4
Shashwat Singh <shashwat0045@gmail.com>
2025-09-16 13:19:47 +0530

    CSES dp

--- dynamic_programming/Edit_Distance.cpp
+++ dynamic_programming/Edit_Distance.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
+#include <string>
+#include <set>
 using namespace std;
 
-class Solution {
-public:
+int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+    if(i<0) return j+1;
+    if(j<0) return i+1;
 
-    int solve(int i, int j, string s1, string s2, vector<vector<int>> &dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+    if(dp[i][j]!= -1) return dp[i][j];
 
-        if(dp[i][j]!= -1) return dp[i][j];
-
-        if(s1[i] == s2[j]){
-            return dp[i][j] = solve(i-1, j-1, s1, s2, dp);
-        }
-        return dp[i][j] = 1+ min({solve(i, j-1, s1, s2, dp), solve(i-1, j, s1, s2, dp), solve(i-1, j-1, s1, s2, dp)});
+    if(s1[i] == s2[j]){
+        return dp[i][j] = solve(i-1, j-1, s1, s2, dp);
     }
-
-    int minDistance(string s1, string s2) {
-        vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), -1));
-        return solve(s1.size()-1, s2.size()-1, s1, s2, dp);
-    }
-};
+    return dp[i][j] = 1+ min({solve(i, j-1, s1, s2, dp), solve(i-1, j, s1, s2, dp), solve(i-1, j-1, s1, s2, dp)});
+}
+int main() {
+    string s1, s2;
+    cin>>s1>>s2;
+    int n = s1.size();
+    int m = s2.size();
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    cout<< solve(n-1, m-1, s1, s2, dp);
+}
