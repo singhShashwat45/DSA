6701e10ada98c4290c276a4892e7fd2a04191984
Shashwat Singh <shashwat0045@gmail.com>
2025-09-24 11:15:51 +0530

    cses binary search

--- binary_search/Find Peak Element.cpp
+++ binary_search/Find Peak Element.cpp
@@ -4,3 +4,22 @@
 #include <string>
 #include <set>
 using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        
+        return l; 
+    }
+};
